d5af5fae5fc5f573a1e3e9ad2cf52b70bf12e104
gtdevice <gtdevice@gmail.com>
2017-04-06 21:36:44 +0300

    Master03 correction logs

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,16 +1,35 @@
 package ru.javawebinar.topjava;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.web.meal.MealRestController;
+
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
+import java.util.List;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
 public class SpringMain {
     public static void main(String[] args) {
         ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
         System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+        adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+        System.out.println();
 
+        MealRestController mealController = appCtx.getBean(MealRestController.class);
+        List<MealWithExceed> filteredMealsWithExceeded =
+                mealController.getBetween(
+                        LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+                        LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+        filteredMealsWithExceeded.forEach(System.out::println);
 //        UserRepository userRepository = (UserRepository) appCtx.getBean("mockUserRepository");
         UserRepository userRepository = appCtx.getBean(UserRepository.class);
         userRepository.getAll();
